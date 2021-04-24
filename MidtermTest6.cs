d0dfd316bfe714b6f5d043e6eb85419b5008775b
Ratchathorn123 <79511320+Ratchathorn123@users.noreply.github.com>
2021-04-24 16:04:31 +0700

    Add files via upload

---
+++ MidtermTest6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MidtermTest_5
+{
+    class Program
+    {
+        static void Stair(int n, char alp)
+        {
+            for (int z = 1; z <= n; z++)
+            {
+                int y;
+                if (z % 2 != 0)
+                    y = z + 1;
+                else
+                    y = z;
+
+                for (int j = 0; j < y; j++)
+                    Console.Write("{0}", alp);
+                Console.WriteLine();
+            }
+        }
+        static void Main()
+        {
+            Char alp;
+            alp = char.Parse(Console.ReadLine());
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            Stair(n, alp);
+        }
+    }
+}
